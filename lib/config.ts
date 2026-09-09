47d521bef1a1649cab0d2d38747850d1f4b1826a
Ariel Poler <ariel@mastil.com>
2026-09-09 09:59:23 -0700

    Clarify application deadline time

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 export const grantConfig = {
   name: 'Choroni West Arts Grant',
   year: 2026,
-  deadline: 'September 25, 2026',
+  deadline: 'September 25, 2026 at 11:59 PM Pacific',
   decisionDate: 'September 30, 2026',
   minGrant: 1000,
   maxGrant: 25000,
